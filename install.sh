3b47060d2fc56456fab6789f8fc883832790d12e
Nimit Kalra <me@nimit.io>
2016-01-06 17:33:02 -0600

    Update installation script.

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 install() {
 	printf "\e[33m[~] Downloading script...\e[0m\n"
 
-	curl -L#o /var/tmp/gg https://raw.githubusercontent.com/qw3rtman/gg/master/bin/gg
-	curl -L#o /var/tmp/gg.1 https://raw.githubusercontent.com/qw3rtman/gg/master/man/gg.1
+	curl -L#o /var/tmp/gg_$$ https://raw.githubusercontent.com/qw3rtman/gg/master/bin/gg
+	curl -L#o /var/tmp/gg_$$.1 https://raw.githubusercontent.com/qw3rtman/gg/master/man/gg.1
 
 	printf "\n\e[33m[~] Setting permissions...\e[0m\n"
 
-	chmod -v +x /var/tmp/gg
+	chmod -v +x /var/tmp/gg_$$
 
 	echo
 
 	printf "\e[33m[~] Moving to \$PATH...\e[0m\n"
 
-	sudo mv -fv /var/tmp/gg /usr/local/bin/gg
-	sudo mv -fv /var/tmp/gg.1 /usr/local/share/man/man1/gg.1
+	sudo mv -fv /var/tmp/gg_$$ /usr/local/bin/gg
+	sudo mv -fv /var/tmp/gg_$$.1 /usr/local/share/man/man1/gg.1
 
 	echo
 
 	version=($(gg -V))
-	
+
 	printf "\e[32m[✔] Successfully installed Git Goodies v${version[3]}\e[32m!\n"
 }
 
